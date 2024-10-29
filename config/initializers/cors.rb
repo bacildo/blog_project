8314619b06ba932b216e84dff373bc2241516f57
bacildo <dbacildodasilva@gmail.com>
2024-10-28 21:27:14 -0300

    feat: added changes to web deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    origins 'http://localhost:3001', ' https://blog-project-ck1o.onrender.com'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
